cdc1d8228338b3cdad42da1db7d70b8748635ddb
singhhardy <singhhardy724@gmail.com>
2023-11-14 21:05:51 +0530

    app fix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from '../src/components/Header';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './Pages/Home';
+import Footer from './components/Footer';
+import Spinner from './components/Spinner';
+import { useEffect, useState } from 'react';
+import Tutorials from './Pages/Tutorials';
+import Blogs from './Pages/Blogs';
+import Contact from './pages/Contact';
 
 function App() {
+  const  [isLoading, setIsLoading] = useState(true)
+
+
+
+  useEffect(() => {
+    window.addEventListener('load', () => {
+      setTimeout(() => {
+        setIsLoading(false);
+        }, 1000);
+      });
+
+    return () => {
+      window.removeEventListener('load', () => {
+        setTimeout(() => {
+          setIsLoading(false);
+        }, 1000);
+      });
+    }
+  }, []);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Header/>
+        {isLoading ? (
+              <Spinner/>
+        ) : (
+          <BrowserRouter>
+          <div className='app mt-5 pt-5'>
+            <Routes>
+              <Route path='/' element={<Home />}/>
+              <Route path='/tutorials' element={<Tutorials />}/>
+              <Route path='/blogs' element={<Blogs />}/>
+              <Route path='/contact' element={<Contact />}/>
+            </Routes>
+          </div>
+        </BrowserRouter>
+        )}
+      <Footer />
+      </>
+  )
 }
 
-export default App;
+export default App
