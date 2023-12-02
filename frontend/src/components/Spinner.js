893cb2b2f86ba0c095f102961c1da24cba9a9b0c
singhhardy <singhhardy724@gmail.com>
2023-12-02 15:37:29 +0530

    new loader

--- frontend/src/components/Spinner.js
+++ frontend/src/components/Spinner.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Spinner() {
   return (
     <div className='spinner'>
-      <video className='spinner-video' src='spinner-dev.mp4' autoPlay loop muted playsInline></video>
+      <img className='spinner-video' src='spinner-dev.gif' />
     </div>
   );
 }
